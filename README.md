a6f9b5c711b93b4348d9e2a0c191df3539abfb57
Nicácio Oliveira <nnicacio@gmail.com>
2022-01-17 11:15:12 -0300

    init repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# Empreenday GH actions
---
+++ src/app/index.ts
@@ -0,0 +1,67 @@
+import express, { json, IRoute } from "express";
+import http from "http";
+
+import { config as setupEnv } from "dotenv";
+
+setupEnv();
+
+export const getEnvVar = (n: string) => process.env[n];
+const app = express();
+export const httpServer = http.createServer(app);
+
+const configDependencies = () => {
+  app.use(json());
+};
+
+const configRoutes = () => {
+  let routes = [];
+
+  app.get("/health", (_req, res) => {
+    res.sendStatus(200);
+  });
+
+  // subroute example
+
+  const subRouter = express();
+  app.get("/subRoute", subRouter);
+
+  subRouter.get("/health2", (_req, res) => {
+    res.sendStatus(200);
+  });
+
+  subRouter.use("*", (_req, res) => {
+    routes = app.routes;
+    res.json(getRoutes(subRouter._router.stack));
+  });
+
+  app.use("*", (_req, res) => {
+    routes = app.routes;
+    res.json(getRoutes(app._router.stack));
+  });
+};
+
+const getRoutes = (stack: any, routes: string[] = []) => {
+  stack.map((l) => {
+    const r: IRoute = l.route;
+    if (r?.path) {
+      routes.push(r.path);
+    }
+  });
+  return routes;
+};
+
+export async function init() {
+  // starts
+  configDependencies();
+  configRoutes();
+
+  httpServer.listen(getEnvVar("PORT"), () =>
+    console.log(`Api running on port: ${getEnvVar("PORT")}`)
+  );
+}
+
+export async function close() {
+  httpServer?.close();
+}
+
+export default app;
